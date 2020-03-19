3c948a1491651d6180b2d171ea15a3b8f456769f
Augustine Hill <gusrh7@rlogin>
2020-03-19 14:39:10 -0400

    person contract start

--- Spot-Block/backend-blockchain/contracts/person.cpp
+++ Spot-Block/backend-blockchain/contracts/person.cpp
@@ -7,14 +7,34 @@ class [[eosio::contract]] person : public contract {
         using contract::contract;
 
         [[eosio::action]]
-        void bid(name spot, int bidAmmount) {
+        void bid(std::string spotID, int bidAmount) {
             //bit on a spot
-
+            
         }
         
         [[eosio::action]]
-        void pay(name reciever, int ammount) {
+        void pay(name userID, person reveiver, int amount) {
             //transfer payment
-
+            //check auth and take amount
+            require_auth(userID);
+            this.funds -= amount;
+            
+            //find person, give money
+            receiver.funds += amount;
         }
+
+        person(name receiver, name code, datastream<const char*> ds):contract(receiver, code, ds) {}
+
+    private:
+        struct [[eosio::table]] user {
+            name ID;
+            std::string spot;
+            int funds;
+
+            uint64_t primary_key () const {
+                return ID;
+            }
+        };
+
+        typedef eosio::multi_index<"users"_n, user> user_index;
 };
