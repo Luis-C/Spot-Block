27536bc816eeff690ab3a4ac8e44a155ca9cb4f9
Revalot <exalt@vt.edu>
2020-03-24 14:47:44 -0400

    Added home with navbar and temp auth-service

--- Spot-Block/src/app/_services/auth.service.ts
+++ Spot-Block/src/app/_services/auth.service.ts
@@ -1,12 +1,44 @@
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 import { Person } from "../_models/person";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService {
+  private currentUserSubject: BehaviorSubject<Person>;
   public currentUser: Observable<Person>;
 
-  constructor() {}
+  constructor() {
+    this.currentUserSubject = new BehaviorSubject<Person>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): Person {
+    return this.currentUserSubject.value;
+  }
+
+  login(id: string) {
+    return new Observable(subscriber => {
+      // if() { //valid login
+      const person: Person = {id, funds: 0, spot: null};
+      localStorage.setItem('currentUser', JSON.stringify(person));
+      // setTimeout(() => {
+      subscriber.next('Logged in!');
+      this.currentUserSubject.next(person);
+      // }, 1000);
+      // } else {
+      //   //reject
+      //   setTimeout(() => {
+      //     subscriber.error('invalid');
+      //   }, 1000);
+      // }
+    });
+  }
+
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
 }
--- Spot-Block/src/app/app.component.ts
+++ Spot-Block/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from "@angular/core";
+import { NotificationsService } from "./_services/notifications.service";
+import {AuthService} from "./_services/auth.service";
+import {Person} from "./_models/person";
+import {Router} from "@angular/router";
 
 @Component({
   selector: "app-root",
@@ -7,4 +11,19 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "Spot-Block";
+  currentUser: Person;
+
+  constructor(private router: Router, private notif: NotificationsService, private auth: AuthService) {
+    this.auth.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+  get isUser() {
+    return this.currentUser;
+  }
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+    // this.notif.notImplemented();
+  }
 }
