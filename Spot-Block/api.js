9096f20e5cb1fec32dace284d50fd3d21295db9a
Luis Hernandez <ziul.ch@gmail.com>
2020-04-02 13:56:45 -0400

    Update api.js
    - Refactored (using Prettier)
    - added a missing comma

--- Spot-Block/api.js
+++ Spot-Block/api.js
@@ -1,21 +1,21 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 var chain_api = express();
 
-chain_api.use(bodyParser.urlencoded({extended: true}));
+chain_api.use(bodyParser.urlencoded({ extended: true }));
 chain_api.use(bodyParser.json());
 
-const { Api, JsonRpc, RpcError } = require('eosjs');
-const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');      // development only
-const fetch = require('node-fetch');                                    // node only; not needed in browsers
-const { TextEncoder, TextDecoder } = require('util');                   // node only; native TextEncoder/Decoder
+const { Api, JsonRpc, RpcError } = require("eosjs");
+const { JsSignatureProvider } = require("eosjs/dist/eosjs-jssig"); // development only
+const fetch = require("node-fetch"); // node only; not needed in browsers
+const { TextEncoder, TextDecoder } = require("util"); // node only; native TextEncoder/Decoder
 
-const rpc = new JsonRpc('http://127.0.0.1:8888', { fetch });
+const rpc = new JsonRpc("http://127.0.0.1:8888", { fetch });
 
 /*
  * test it is running
  */
-chain_api.get('/test', function (req, res) {
+chain_api.get("/test", function(req, res) {
   console.log("test");
   res.end("test");
 });
@@ -24,33 +24,45 @@ chain_api.get('/test', function (req, res) {
  * fields should be key, user, account, fund, and spot
  * user must be spotblock
  */
-chain_api.post('/createUser', async(req, res) => {
+chain_api.post("/createUser", async (req, res) => {
   console.log("Call: createUser");
   const signatureProvider = new JsSignatureProvider([req.body.key]);
-  const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+  const api = new Api({
+    rpc,
+    signatureProvider,
+    textDecoder: new TextDecoder(),
+    textEncoder: new TextEncoder()
+  });
 
   try {
-    const result = await api.transact({
-      actions: [{
-        account: 'spotblock',
-        name: 'createuser',
-        authorization: [{
-          actor: req.body.user,
-          permission: 'active',
-        }],
-        data: {
-          accountID: req.body.account,
-          initialFunds: req.body.funds,
-          spotID: req.body.spot,
-       },
-      }]
-    }, {
-      blocksBehind: 3,
-      expireSeconds: 30,
-    });
+    const result = await api.transact(
+      {
+        actions: [
+          {
+            account: "spotblock",
+            name: "createuser",
+            authorization: [
+              {
+                actor: req.body.user,
+                permission: "active"
+              }
+            ],
+            data: {
+              accountID: req.body.account,
+              initialFunds: req.body.funds,
+              spotID: req.body.spot
+            }
+          }
+        ]
+      },
+      {
+        blocksBehind: 3,
+        expireSeconds: 30
+      }
+    );
     console.log("account added");
     res.end("Account is added");
-  } catch(e) {
+  } catch (e) {
     console.log("error");
     console.log(e.message);
     res.end(JSON.stringify(e.json, null, 2));
@@ -61,34 +73,46 @@ chain_api.post('/createUser', async(req, res) => {
  * fields should be key, user, id, owner, lot, and coord
  * user must be spotblock
  */
-chain_api.post('/createSpot', async(req, res) => {
+chain_api.post("/createSpot", async (req, res) => {
   console.log("Call: createSpot");
   const signatureProvider = new JsSignatureProvider([req.body.key]);
-  const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+  const api = new Api({
+    rpc,
+    signatureProvider,
+    textDecoder: new TextDecoder(),
+    textEncoder: new TextEncoder()
+  });
 
   try {
-    const result = await api.transact({
-      actions: [{
-        account: 'spotblock',
-        name: 'createspot',
-        authorization: [{
-          actor: req.body.user,
-          permission: 'active',
-        }],
-        data: {
-          id: req.body.id,
-          owner: req.body.owner,
-          lot: req.body.lot,
-          coord: req.body.coord,
-       },
-      }]
-    }, {
-      blocksBehind: 3,
-      expireSeconds: 30,
-    });
+    const result = await api.transact(
+      {
+        actions: [
+          {
+            account: "spotblock",
+            name: "createspot",
+            authorization: [
+              {
+                actor: req.body.user,
+                permission: "active"
+              }
+            ],
+            data: {
+              id: req.body.id,
+              owner: req.body.owner,
+              lot: req.body.lot,
+              coord: req.body.coord
+            }
+          }
+        ]
+      },
+      {
+        blocksBehind: 3,
+        expireSeconds: 30
+      }
+    );
     console.log("spot added");
     res.end("Spot is added");
-  } catch(e) {
+  } catch (e) {
     console.log("error");
     console.log(e.message);
     res.end(e.message);
@@ -98,32 +122,44 @@ chain_api.post('/createSpot', async(req, res) => {
 /*
  *  fields should be key, user, spotid and use_time
  */
-chain_api.post('/createAuc', async(req, res) => {
+chain_api.post("/createAuc", async (req, res) => {
   console.log("Call: createAuc");
   const signatureProvider = new JsSignatureProvider([req.body.key]);
-  const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+  const api = new Api({
+    rpc,
+    signatureProvider,
+    textDecoder: new TextDecoder(),
+    textEncoder: new TextEncoder()
+  });
 
   try {
-    const result = await api.transact({
-      actions: [{
-        account: 'spotblock',
-        name: 'createauc',
-        authorization: [{
-          actor: req.body.user,
-          permission: 'active',
-        }],
-        data: {
-          spotid: req.body.spotid,
-          use_time: req.body.use_time,
-       },
-      }]
-    }, {
-      blocksBehind: 3,
-      expireSeconds: 30,
-    });
+    const result = await api.transact(
+      {
+        actions: [
+          {
+            account: "spotblock",
+            name: "createauc",
+            authorization: [
+              {
+                actor: req.body.user,
+                permission: "active"
+              }
+            ],
+            data: {
+              spotid: req.body.spotid,
+              use_time: req.body.use_time
+            }
+          }
+        ]
+      },
+      {
+        blocksBehind: 3,
+        expireSeconds: 30
+      }
+    );
     console.log("auction added");
     res.end("Auction is added");
-  } catch(e) {
+  } catch (e) {
     console.log("error");
     console.log(e.message);
     res.end(e.message);
@@ -133,32 +169,44 @@ chain_api.post('/createAuc', async(req, res) => {
 /*
  * fields should be key, user, accountid, and spotid
  */
-chain_api.post('/assignSpot', async(req, res) => {
+chain_api.post("/assignSpot", async (req, res) => {
   console.log("Call: assignSpot");
   const signatureProvider = new JsSignatureProvider([req.body.key]);
-  const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+  const api = new Api({
+    rpc,
+    signatureProvider,
+    textDecoder: new TextDecoder(),
+    textEncoder: new TextEncoder()
+  });
 
   try {
-    const result = await api.transact({
-      actions: [{
-        account: 'spotblock',
-        name: 'assignspot',
-        authorization: [{
-          actor: req.body.user,
-          permission: 'active',
-        }],
-        data: {
-          accountID: req.body.accountid,
-          spotID: req.body.spotid,
-       },
-      }]
-    }, {
-      blocksBehind: 3,
-      expireSeconds: 30,
-    });
+    const result = await api.transact(
+      {
+        actions: [
+          {
+            account: "spotblock",
+            name: "assignspot",
+            authorization: [
+              {
+                actor: req.body.user,
+                permission: "active"
+              }
+            ],
+            data: {
+              accountID: req.body.accountid,
+              spotID: req.body.spotid
+            }
+          }
+        ]
+      },
+      {
+        blocksBehind: 3,
+        expireSeconds: 30
+      }
+    );
     console.log("Spot assigned");
     res.end("Spot assigned.");
-  } catch(e) {
+  } catch (e) {
     console.log("error");
     console.log(e.message);
     res.end(e.message);
@@ -168,33 +216,45 @@ chain_api.post('/assignSpot', async(req, res) => {
 /*
  * fields should be key, user, userid, auctionid, bidamount
  */
-chain_api.post('/bid', async(req, res) => {
+chain_api.post("/bid", async (req, res) => {
   console.log("Call: bid");
   const signatureProvider = new JsSignatureProvider([req.body.key]);
-  const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+  const api = new Api({
+    rpc,
+    signatureProvider,
+    textDecoder: new TextDecoder(),
+    textEncoder: new TextEncoder()
+  });
 
   try {
-    const result = await api.transact({
-      actions: [{
-        account: 'spotblock',
-        name: 'bid',
-        authorization: [{
-          actor: req.body.user,
-          permission: 'active',
-        }],
-        data: {
-          userID: req.body.userid,
-          auctionID: req.body.auctionid,
-          bidAmount: req.body.bidamount,
-       },
-      }]
-    }, {
-      blocksBehind: 3,
-      expireSeconds: 30,
-    });
+    const result = await api.transact(
+      {
+        actions: [
+          {
+            account: "spotblock",
+            name: "bid",
+            authorization: [
+              {
+                actor: req.body.user,
+                permission: "active"
+              }
+            ],
+            data: {
+              userID: req.body.userid,
+              auctionID: req.body.auctionid,
+              bidAmount: req.body.bidamount
+            }
+          }
+        ]
+      },
+      {
+        blocksBehind: 3,
+        expireSeconds: 30
+      }
+    );
     console.log("Bid placed");
     res.end("Bid placed.");
-  } catch(e) {
+  } catch (e) {
     console.log("error");
     console.log(e.message);
     res.end(e.message);
@@ -204,35 +264,48 @@ chain_api.post('/bid', async(req, res) => {
 /*
  * fields should be key, userID, receiverID, amount
  */
-chain_api.post('/pay', async function (req, res) {
+chain_api.post("/pay", async function(req, res) {
   console.log("Call: Pay");
   const signatureProvider = new JsSignatureProvider([req.body.key]);
-  const api = new Api({rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+  const api = new Api({
+    rpc,
+    signatureProvider,
+    textDecoder: new TextDecoder(),
+    textEncoder: new TextEncoder()
+  });
 
   try {
-    const result = await api.transact({
-      actions: [{
-        account: 'spotblock',
-        name: 'pay',
-        authorization: [{
-          actor: req.body.userID,
-          permission: 'active',
-        }, {
-          actor: req.body.receiverID
-          permission: 'active',
-        }],
-        data: {
-          userID: req.body.userID,
-          receiverID: req.body.receiverID,
-          amount: req.body.amount,
-        }
-      }]
-    }, {
-      blocksBehind: 3,
-      expireSeconds: 30,
-    });
+    const result = await api.transact(
+      {
+        actions: [
+          {
+            account: "spotblock",
+            name: "pay",
+            authorization: [
+              {
+                actor: req.body.userID,
+                permission: "active"
+              },
+              {
+                actor: req.body.receiverID,
+                permission: "active"
+              }
+            ],
+            data: {
+              userID: req.body.userID,
+              receiverID: req.body.receiverID,
+              amount: req.body.amount
+            }
+          }
+        ]
+      },
+      {
+        blocksBehind: 3,
+        expireSeconds: 30
+      }
+    );
     res.end("Paid");
-  } catch(e) {
+  } catch (e) {
     console.log("error");
     console.log(e.message);
     res.end(e.message);
