b3c8fc28372acca742b9c81bb5deecfa85a7c23a
Luis Hernandez <ziul.ch@gmail.com>
2020-04-07 14:02:00 -0400

    Update home.component.ts

--- Spot-Block/src/app/home/home.component.ts
+++ Spot-Block/src/app/home/home.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BlockchainService } from "../_services/blockchain.service";
+import { Spot } from "../_models/spot";
+import { AuthService } from "../_services/auth.service";
+import { Auction } from "../_models/auction";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  spots: Spot[];
+  auctions: Auction[];
+  currentUser: string;
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private blockchain: BlockchainService
+  ) {}
 
   ngOnInit(): void {
+    this.currentUser = this.auth.currentUserValue.ID;
+    this.load().then((r) => console.log(r));
   }
 
+  async load() {
+    this.spots = await this.blockchain.query_table("spots", 0, undefined);
 
+    this.auctions = await this.blockchain.query_table("auctions", 0, undefined);
+  }
 }
