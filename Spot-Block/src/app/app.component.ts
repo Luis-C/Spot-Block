38a38fb481020886d18665de21a3c2ee1fa9177c
Revalot <exalt@vt.edu>
2020-04-07 12:00:48 -0400

    Update home

--- Spot-Block/src/app/app.component.ts
+++ Spot-Block/src/app/app.component.ts
@@ -1,24 +1,28 @@
-import { Component } from "@angular/core";
-import { NotificationsService } from "./_services/notifications.service";
-import {AuthService} from "./_services/auth.service";
-import {Person} from "./_models/person";
-import {Router} from "@angular/router";
+import { Component } from '@angular/core';
+import { NotificationsService } from './_services/notifications.service';
+import { AuthService } from './_services/auth.service';
+import { Person } from './_models/person';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = "Spot-Block";
+  title = 'Spot-Block';
   currentUser: Person;
   homeDisabled: boolean;
   purchaseDisabled: boolean;
   bidDisabled: boolean;
   sellDisabled: boolean;
 
-  constructor(private router: Router, private notif: NotificationsService, private auth: AuthService) {
-    this.auth.currentUser.subscribe(x => this.currentUser = x);
+  constructor(
+    private router: Router,
+    private notif: NotificationsService,
+    private auth: AuthService
+  ) {
+    this.auth.currentUser.subscribe((x) => (this.currentUser = x));
     this.homeDisabled = true;
   }
 
@@ -47,14 +51,18 @@ export class AppComponent {
     this.sellDisabled = false;
   }
 
-  sell() {
-    this.homeDisabled = false;
-    this.purchaseDisabled = false;
-    this.bidDisabled = false;
-    this.sellDisabled = true;
-  }
+  // sell() {
+  //   this.homeDisabled = false;
+  //   this.purchaseDisabled = false;
+  //   this.bidDisabled = false;
+  //   this.sellDisabled = true;
+  // }
 
   logout() {
+    this.homeDisabled = true;
+    this.purchaseDisabled = false;
+    this.bidDisabled = false;
+    this.sellDisabled = false;
     this.auth.logout();
     this.router.navigate(['/login']);
     // this.notif.notImplemented();
--- Spot-Block/src/app/home/home.component.ts
+++ Spot-Block/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {BlockchainService} from '../_services/blockchain.service';
+import {Spot} from '../_models/spot';
+import {AuthService} from '../_services/auth.service';
+import {Auction} from '../_models/auction';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  spots: Spot[];
+  auctions: Auction[];
+  currentUser: string;
 
-  constructor() { }
+  constructor(private auth: AuthService, private blockchain: BlockchainService) { }
 
   ngOnInit(): void {
+    this.currentUser = this.auth.currentUserValue.ID;
+    this.load().then(r => console.log(r));
   }
 
+  async load() {
+    this.spots = await this.blockchain.query_table(
+      'spots',
+      0,
+      undefined
+    );
+
+    this.auctions = await this.blockchain.query_table(
+      'auctions',
+      0,
+      undefined
+    );
+  }
 
 }
