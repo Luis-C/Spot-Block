c4e2f07766dcb4da299d8daec993b7ba8c0d3b45
Luis Hernandez <ziul.ch@gmail.com>
2020-04-21 19:46:35 -0400

    Deleted docs and Spot-Block
    - to facilitate merging

--- Spot-Block/src/app/_models/person.ts
+++
@@ -1,7 +0,0 @@
-import { Spot } from "./spot";
-
-export interface Person {
-  id: string;
-  funds: number;
-  spot: any;
-}
--- Spot-Block/src/app/_models/spot.ts
+++
@@ -1,9 +0,0 @@
-import { Person } from "./person";
-
-export interface Spot {
-  lot: string;
-  coord: string;
-  owner: Person | null;
-  current_bid: number;
-  rent_time: number;
-}
