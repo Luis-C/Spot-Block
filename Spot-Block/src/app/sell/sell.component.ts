20efc53c7a0ed87d602916b90fbe4c456359d552
Luis Hernandez <ziul.ch@gmail.com>
2020-04-30 17:15:40 -0400

    Update sell.component.ts

--- Spot-Block/src/app/sell/sell.component.ts
+++ Spot-Block/src/app/sell/sell.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
+/**
+ * This has been deprecated and should be deleted
+ * We use search component instead
+ * @deprecated
+ */
 @Component({
-  selector: 'app-sell',
-  templateUrl: './sell.component.html',
-  styleUrls: ['./sell.component.scss']
+  selector: "app-sell",
+  templateUrl: "./sell.component.html",
+  styleUrls: ["./sell.component.scss"],
 })
 export class SellComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
